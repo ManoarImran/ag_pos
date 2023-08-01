0a0f37210d497e4843fa1980cc638c8e13d2c656
Md-Manoar-Imran <monoarimran@gmail.com>
2023-08-01 15:22:45 +0600

    HomePage Button Added

--- lib/Model/Home.dart
+++ lib/Model/Home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Inventory/InventoryPage.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF001C30),
-        title:  const Center(
+        title: const Center(
           child: Text(
             'HOME',
             style: TextStyle(
@@ -24,7 +26,6 @@ class HomePage extends StatelessWidget {
   }
 }
 
-
 class HomePageBody extends StatefulWidget {
   const HomePageBody({Key? key}) : super(key: key);
 
@@ -35,6 +36,92 @@ class HomePageBody extends StatefulWidget {
 class _HomePageBodyState extends State<HomePageBody> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green,);
+    return Container(
+      child: GridView.count(
+        crossAxisCount: 3,
+        children: [
+          buildGridButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => InventoryPage()));
+            },
+            color: Colors.amber,
+            imageAsset: 'assets/Inventory.png',
+            label: "Inventory",
+          ),
+          buildGridButton(
+            onPressed: () {
+              // Code for Sales button's onPressed
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => const LeaveApplication()));
+            },
+            color: Colors.red,
+            imageAsset: 'assets/sales.png',
+            label: "Sales",
+          ),
+          buildGridButton(
+            onPressed: () {
+              // Code for Accounts button's onPressed
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceReportPage()));
+            },
+            color: Colors.green,
+            imageAsset: 'assets/accounts.png',
+            label: "Accounts",
+          ),
+          buildGridButton(
+            onPressed: () {
+              // Code for Reports button's onPressed
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => const MovementApplicationPage()));
+            },
+            color: Colors.deepPurple,
+            imageAsset: 'assets/report.png',
+            label: "Reports",
+          ),
+          buildGridButton(
+            onPressed: () {
+              // Code for Purchase button's onPressed
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => const DealerOrder()));
+            },
+            color: Colors.indigo,
+            imageAsset: 'assets/purchase.png',
+            label: "Purchase",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGridButton({
+    required VoidCallback onPressed,
+    required Color color,
+    required String imageAsset,
+    required String label,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+            backgroundColor: MaterialStateColor.resolveWith((states) => color)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset(imageAsset),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(2, 0, 3, 4),
+              child: Text(
+                label,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
